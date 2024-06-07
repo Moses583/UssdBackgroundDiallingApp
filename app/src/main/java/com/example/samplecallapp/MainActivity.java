56eb15ab924c7ba3e9cd18641a816f78821957eb
Moses583 <moisendungu@gmail.com>
2024-06-07 12:22:09 +0300

    Fixed dialing issue

--- app/src/main/java/com/example/samplecallapp/MainActivity.java
+++ app/src/main/java/com/example/samplecallapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.samplecallapp;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -16,8 +29,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Spinner spinner;
+    private Map<Integer, Integer> simMap;
+    private ArrayList<String> simNames;
+    private ArrayList<Integer> slotIndex;
+    String[] permissions = new String[]{
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.READ_PHONE_STATE,
+
+    };
+    TelephonyManager manager;
+    TelephonyManager.UssdResponseCallback callback;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +61,88 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},101);
-        }
+
+
+        simMap = new HashMap<>();
+        simNames = new ArrayList<>();
+        slotIndex = new ArrayList<>();
+
         button = findViewById(R.id.callNumber);
+        spinner = findViewById(R.id.selectSim);
+
+        listSimInfo();
+
+        manager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            callback = new TelephonyManager.UssdResponseCallback() {
+                @Override
+                public void onReceiveUssdResponse(TelephonyManager telephonyManager, String request, CharSequence response) {
+                    super.onReceiveUssdResponse(telephonyManager, request, response);
+                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onReceiveUssdResponseFailed(TelephonyManager telephonyManager, String request, int failureCode) {
+                    super.onReceiveUssdResponseFailed(telephonyManager, request, failureCode);
+                }
+            };
+        }
+        handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+            }
+        };
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String number = "*544" + Uri.encode("#");
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:"+number));
-                startActivity(intent);
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},101);
+                    return;
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    String dialSim1;
+                    int subscriptionId = 0;
+                    dialSim1 = spinner.getSelectedItem().toString();
+                    if (dialSim1.contains("SIM: 1")){
+                        subscriptionId = simMap.get(0);
+                    }else if(dialSim1.contains("SIM: 2")){
+                        subscriptionId = simMap.get(1);
+                    }
+                    manager.createForSubscriptionId(subscriptionId).sendUssdRequest("*100*4*1*1#",callback,handler);
+
+                }
             }
         });
 
     }
+
+    public void listSimInfo(){
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.CALL_PHONE},101);
+            return;
+        }
+        List<SubscriptionInfo> infoList = SubscriptionManager.from(MainActivity.this).getActiveSubscriptionInfoList();
+        for (SubscriptionInfo info:
+                infoList) {
+            simMap.put(info.getSimSlotIndex(),info.getSubscriptionId());
+            String slot = String.valueOf(info.getSimSlotIndex()+1);
+            simNames.add(info.getCarrierName().toString()+" SIM: "+slot);
+            slotIndex.add(info.getSimSlotIndex());
+        }
+        ArrayAdapter adapter1 = new ArrayAdapter(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,simNames);
+        spinner.setAdapter(adapter1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 101){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                listSimInfo();
+            }
+        }
+    }
+
+
 }
